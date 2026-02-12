d56513553f2bc4bf56b964a0a41d7a20f3dfd07e
Hijin554 <dhgmlwls86@gmil.com>
2026-02-12 10:58:07 +0900

    deploy test1

---
+++ src/utils/apiUrls.js
@@ -0,0 +1,15 @@
+const DOMAIN = 'http://34.233.120.69';
+
+const GET_TASKS_API_URL = `${DOMAIN}/get_tasks`;
+const POST_TASK_API_URL = `${DOMAIN}/post_task`;
+const UPDATE_COMPLETED_TASK_API_URL = `${DOMAIN}/update_completed_task`;
+const DELETE_TASK_API_URL = `${DOMAIN}/delete_task`;
+const UPDATE_TASK_API_URL = `${DOMAIN}/update_task`;
+
+export {
+  GET_TASKS_API_URL,
+  POST_TASK_API_URL,
+  UPDATE_COMPLETED_TASK_API_URL,
+  DELETE_TASK_API_URL,
+  UPDATE_TASK_API_URL,
+};
